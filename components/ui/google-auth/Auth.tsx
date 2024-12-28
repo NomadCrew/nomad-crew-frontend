aa2f87fced9454b9ae332c7c49293104e8ad8fa6
Naqeebali Naveed Shamsi <nq957544@dal.ca>
2024-12-28 14:44:51 -0500

    Integrate supabase auth

--- components/ui/google-auth/Auth.tsx
+++ components/ui/google-auth/Auth.tsx
@@ -1,6 +1,7 @@
 import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
 import { useTheme } from '@/src/theme';
 import { getButtonStyles } from '@/src/theme/styles';
+import {supabase} from '@/src/auth/supabaseClient';
 
 export default function AuthButton() {
   GoogleSignin.configure({
@@ -17,25 +18,30 @@ export default function AuthButton() {
       size={GoogleSigninButton.Size.Wide}
       color={GoogleSigninButton.Color.Dark}
       onPress={async () => {
-        try{
-          console.log(process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID);
-          await GoogleSignin.hasPlayServices();
-          const userInfo = await GoogleSignin.signIn();
-          console.log(JSON.stringify(userInfo, null, 2));
+        try {
+          await GoogleSignin.hasPlayServices()
+          const userInfo = await GoogleSignin.signIn()
+          if (userInfo.data && userInfo.data.idToken) {
+            const { data, error } = await supabase.auth.signInWithIdToken({
+              provider: 'google',
+              token: userInfo.data.idToken,
+            })
+            console.log(error, data)
+          } else {
+            throw new Error('no ID token present!')
+          }
         } catch (error: any) {
           if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-            console.log('User cancelled the sign in process');
+            // user cancelled the login flow
           } else if (error.code === statusCodes.IN_PROGRESS) {
-            console.log('Sign in is in progress');
+            // operation (e.g. sign in) is in progress already
           } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-            console.log('Play services not available or outdated');
+            // play services not available or outdated
           } else {
-            console.error('Something went wrong:', error);
-      }
-    }
-  }
-}
-      // disabled={this.state.isSigninInProgress}
+            // some other error happened
+          }
+        }
+      }}
     />
   );
 }
